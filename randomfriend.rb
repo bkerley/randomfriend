0912cf81e88cfe3a5b4fe3031b78d1a622b2971d
Bryce Kerley <bkerley@brycekerley.net>
2010-09-24 17:20:45 -0400

    send user to twitter to authorize

--- randomfriend.rb
+++ randomfriend.rb
@@ -2,3 +2,9 @@ require 'twitter'
 
 # consumer key, consumer secret
 oauth = Twitter::OAuth.new('wK4SHcQXL3EhCtihararow', 'f91TDJV1Gn0Emr7OYMNZ0ASPjQ3nrNiM4SlPuVm5Uk')
+
+request_token = oauth.request_token.token
+request_secret= oauth.request_token.secret
+
+puts "Please visit this address to authorize me:"
+puts oauth.request_token.authorize_url
